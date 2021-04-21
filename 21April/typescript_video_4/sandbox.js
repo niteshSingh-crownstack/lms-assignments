07b104078bf0f06c2f91ac877d1b94c199578e0f
Nitesh Singh <niteshsingh7463@gmail.com>
2021-04-21 13:53:53 +0530

    Dynamic(any)

---
+++ 21April/typescript_video_4/sandbox.js
@@ -0,0 +1,17 @@
+var age = 25;
+age = true;
+console.log(age);
+age = 'hello';
+console.log(age);
+age = { name: 'lugi' };
+console.log(age);
+var mixed = [];
+mixed.push(5);
+mixed.push('mario');
+mixed.push(false);
+console.log(mixed);
+var ninja;
+ninja = { name: 'yoshi', age: 34 };
+console.log(ninja);
+ninja = { name: 78, age: 'yoshi' };
+console.log(ninja);
---
+++ 21April/typescript_video_4/sandbox.ts
@@ -0,0 +1,23 @@
+let age:any =25;
+
+age = true;
+console.log(age);
+age='hello';
+console.log(age);
+age = {name:'lugi'};
+console.log(age);
+ 
+let mixed:any[]=[];
+
+mixed.push(5);
+mixed.push('mario');
+mixed.push(false);
+console.log(mixed);
+
+let ninja:{name:any,age:any};
+ninja = {name:'yoshi',age:34};
+console.log(ninja);
+
+ninja = {name:78,age:'yoshi'};
+console.log(ninja);
+
