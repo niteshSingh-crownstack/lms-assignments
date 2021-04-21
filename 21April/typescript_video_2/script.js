9f5f75226577713c3e19e241b8daf4555423b9e3
Nitesh Singh <niteshsingh7463@gmail.com>
2021-04-21 10:53:07 +0530

    Enum

---
+++ 21April/typescript_video_2/script.js
@@ -0,0 +1,10 @@
+var myConstants;
+(function (myConstants) {
+    myConstants[myConstants["pi"] = 3.14] = "pi";
+    myConstants[myConstants["e"] = 2.73] = "e";
+    myConstants[myConstants["log2"] = 0.3] = "log2";
+    myConstants[myConstants["log5"] = 0.7] = "log5";
+})(myConstants || (myConstants = {}));
+console.log('Finding circumference of circle');
+var radius = 10;
+console.log(2 * myConstants.pi * radius);
