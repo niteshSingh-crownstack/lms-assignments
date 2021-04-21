80ceedebde86bb7f5bdb38d0249433127b55cf98
Nitesh Singh <niteshsingh7463@gmail.com>
2021-04-21 19:55:07 +0530

    Classes

---
+++ 21April/typescript_video_7/main.js
@@ -0,0 +1,20 @@
+"use strict";
+exports.__esModule = true;
+var Point = /** @class */ (function () {
+    function Point() {
+    }
+    Point.prototype.draw = function () {
+        //..
+    };
+    Point.prototype.getDistance = function (another) {
+        //...
+    };
+    return Point;
+}());
+// let drawPoint = (point:Point)=>{
+//     //..
+// }
+// drawPoint({
+//     x:1,
+//     y:2
+// })
---
+++ 21April/typescript_video_7/main.ts
@@ -0,0 +1,23 @@
+export{}
+
+class Point{
+    x:number;
+    y:number;
+
+    draw() {
+        //..
+    }
+
+    getDistance(another:Point){
+        //...
+    }
+
+}
+
+// let drawPoint = (point:Point)=>{
+//     //..
+// }
+// drawPoint({
+//     x:1,
+//     y:2
+// })
