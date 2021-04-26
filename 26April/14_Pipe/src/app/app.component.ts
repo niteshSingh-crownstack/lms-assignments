c32548048c3d5ba0976861624b210fbbc5f2d6b7
Nitesh Singh <niteshsingh7463@gmail.com>
2021-04-26 19:30:49 +0530

    Pipe

---
+++ 26April/14_Pipe/src/app/app.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+
+  title='blog';
+name="peter parKEr";
+today=Date.now();
+str="Hello Angular"
+money=100;
+
+}
