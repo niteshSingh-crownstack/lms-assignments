765836c063e7d327b0f8138f67557b8b07969fb5
Nitesh Singh <niteshsingh7463@gmail.com>
2021-04-26 19:34:41 +0530

    show list from api

---
+++ 26April/19_ShowListFromApi/src/app/app.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+import { UsersService } from './users.service';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+
+  title='blog';
+  data:any;
+constructor(private user:UsersService){
+  user.getData().subscribe(data=>{console.log(data);this.data=data;})
+}
+}
