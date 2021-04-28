3a4c95824199946a7c09457d4810ceb20c3fa4c2
Nitesh Singh <niteshsingh7463@gmail.com>
2021-04-28 19:56:58 +0530

    Budget Calculator

---
+++ 28April/BudgetCalculator/src/app/app.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { BrowserModule } from '@angular/platform-browser';
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { MainPageComponent } from './main-page/main-page.component';
+import { AddItemFormComponent } from './add-item-form/add-item-form.component';
+
+@NgModule({
+  declarations: [AppComponent, MainPageComponent, AddItemFormComponent],
+  imports: [BrowserModule, AppRoutingModule, FormsModule],
+  providers: [],
+  bootstrap: [AppComponent],
+})
+export class AppModule {}
