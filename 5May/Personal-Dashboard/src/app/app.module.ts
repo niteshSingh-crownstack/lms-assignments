ca6aecc105bd284006d6caaf03a8a2fa7bdc1a3e
Nitesh Singh <niteshsingh7463@gmail.com>
2021-05-06 01:19:52 +0530

    Personal Dasboard App

---
+++ 5May/Personal-Dashboard/src/app/app.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { TabsComponent } from './tabs/tabs.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    TabsComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
