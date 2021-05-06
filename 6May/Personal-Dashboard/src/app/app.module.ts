2db1ba4eb5be15e544df123022d782576d40e3ad
Nitesh Singh <niteshsingh7463@gmail.com>
2021-05-07 01:23:35 +0530

    Personal Dashboard

--- 6May/Personal-Dashboard/src/app/app.module.ts
+++ 6May/Personal-Dashboard/src/app/app.module.ts
@@ -7,6 +7,8 @@ import { TabsComponent } from './tabs/tabs.component';
 import { BookmarksComponent } from './bookmarks/bookmarks.component';
 import { TodosComponent } from './todos/todos.component';
 import { NotesComponent } from './notes/notes.component';
+import { BookmarkTileComponent } from './bookmark-tile/bookmark-tile.component';
+import { TodoItemComponent } from './todo-item/todo-item.component';
 
 @NgModule({
   declarations: [
@@ -14,7 +16,9 @@ import { NotesComponent } from './notes/notes.component';
     TabsComponent,
     BookmarksComponent,
     TodosComponent,
-    NotesComponent
+    NotesComponent,
+    BookmarkTileComponent,
+    TodoItemComponent
   ],
   imports: [
     BrowserModule,
