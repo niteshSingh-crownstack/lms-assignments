a8291050d88af207bd684bb17135d42b3fe60aee
Nitesh Singh <niteshsingh7463@gmail.com>
2021-05-07 20:03:57 +0530

    personal dashboard

---
+++ 7May/src/app/app.component.ts
@@ -0,0 +1,15 @@
+import { trigger } from '@angular/animations';
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+  animations: [trigger('routeAnim', [])],
+})
+export class AppComponent {
+  prepareRoute(outlet: RouterOutlet) {
+    if (outlet.isActivated) return outlet.activatedRoute.snapshot.url;
+  }
+}
