63796230c5fe8ab4e87a9aaf20a63cc0b61a6ae6
Nitesh Singh <niteshsingh7463@gmail.com>
2021-05-10 01:04:44 +0530

    Personal Dashboard

--- 7May/Personal-Dashboard/src/app/todo-item/todo-item.component.ts
+++ 7May/Personal-Dashboard/src/app/todo-item/todo-item.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { Todo } from '../shared/todo.model';
 
 @Component({
   selector: 'app-todo-item',
@@ -7,9 +8,22 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TodoItemComponent implements OnInit {
 
+  @Input() todo: Todo
+
+  @Output() editClick: EventEmitter<void> = new EventEmitter()
+  @Output() deleteClick: EventEmitter<void> = new EventEmitter()
+
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  onEditClick() {
+    this.editClick.emit()
+  }
+
+  onDeleteClick() {
+    this.deleteClick.emit()
   }
 
 }
