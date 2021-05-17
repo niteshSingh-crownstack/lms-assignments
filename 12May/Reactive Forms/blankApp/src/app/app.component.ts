4b16992439183fa3dfc8223e785f01c9563b551b
Nitesh Singh <niteshsingh7463@gmail.com>
2021-05-17 09:47:42 +0530

    Reactive Forms

---
+++ 12May/Reactive Forms/blankApp/src/app/app.component.ts
@@ -0,0 +1,31 @@
+import { Component } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { zipcodeValidator, passValidator } from './validator';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  title = 'app';
+
+  form: FormGroup;
+  constructor(private fb: FormBuilder) {
+    this.form = this.fb.group({
+      username: ['', [Validators.required, Validators.minLength(4)]],
+      password: '',
+      cnfpass: ['', passValidator],
+      zip: ['', zipcodeValidator],
+    });
+
+    this.form.controls.password.valueChanges.subscribe((x) =>
+      this.form.controls.cnfpass.updateValueAndValidity()
+    );
+  }
+
+  onSubmit() {
+    // console.log(this.form.controls.zip);
+    this.form.markAsTouched();
+  }
+}
