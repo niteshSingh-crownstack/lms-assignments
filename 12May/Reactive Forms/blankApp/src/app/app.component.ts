0f2666f18d69780e29db7d9f866e5f5abfaae558
Nitesh Singh <niteshsingh7463@gmail.com>
2021-05-17 20:52:02 +0530

    added dynamic form using formArray

--- 12May/Reactive Forms/blankApp/src/app/app.component.ts
+++ 12May/Reactive Forms/blankApp/src/app/app.component.ts
@@ -1,5 +1,11 @@
-import { Component } from '@angular/core';
-import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { Component, OnInit } from '@angular/core';
+import {
+  FormGroup,
+  FormBuilder,
+  Validators,
+  FormArray,
+  AbstractControl,
+} from '@angular/forms';
 import { zipcodeValidator, passValidator } from './validator';
 import { takeWhile } from 'rxjs/operators';
 @Component({
@@ -7,27 +13,54 @@ import { takeWhile } from 'rxjs/operators';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'app';
   alive = true;
-  form: FormGroup;
-  constructor(private fb: FormBuilder) {
+  form!: FormGroup;
+
+  constructor(private fb: FormBuilder) {}
+  ngOnInit() {
+    this.formInit();
+  }
+  formInit() {
     this.form = this.fb.group({
       username: ['', [Validators.required, Validators.minLength(4)]],
       password: '',
       cnfpass: ['', passValidator],
       zip: ['', zipcodeValidator],
+      address: this.fb.array([this.addAddressGroup()]),
     });
 
     this.form.controls.password.valueChanges
       .pipe(takeWhile((_) => this.alive))
       .subscribe((x) => this.form.controls.cnfpass.updateValueAndValidity());
   }
+  addAddressGroup() {
+    return this.fb.group({
+      state: [null, Validators.required],
+      city: [null, Validators.required],
+    });
+  }
+
+  get addressArray() {
+    return <any>this.form.get('address');
+  }
+  addAddress() {
+    this.addressArray.push(this.addAddressGroup());
+  }
+  removeAddress(index: number) {
+    this.addressArray.removeAt(index);
+  }
   ngOnDestroy() {
     this.alive = false;
   }
   onSubmit() {
-    // console.log(this.form.controls.zip);
     this.form.markAsTouched();
+    console.log(this.form.value);
+  }
+
+  //for testing
+  showForm() {
+    console.log('worked');
   }
 }
